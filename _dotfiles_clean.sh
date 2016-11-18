769cb287e45c196efbba3bb67842fe0885c44d44
Toby Joe Boudreaux <tj@tobyjoe.com>
2016-11-18 12:02:35 -0500

    Added little cleaner script to make testing easier

---
+++ _dotfiles_clean.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+echo "Resetting $HOME"
+rcdn
+rm -rf .atom
+rm -rf .zprezto
+rm .rcrc
+rm .zcompdump
+rm .zcompdump.zwc
+rm .gitconfig
+rm -rf .dotfiles
+echo ""
+ls -la
+echo "All clean..."
+echo ""
+
+echo "Prepping for setup..."
+export DOTFILES_HOME="$HOME/.dotfiles"
+git clone git://github.com/tobyjoe/dotfiles.git $DOTFILES_HOME
+cd $DOTFILES_HOME
